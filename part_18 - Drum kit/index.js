2bf5bed9297d3c9d150180d43a6eca8b7e83de42
NE-KK <134157533+NE-KK@users.noreply.github.com>
2025-11-07 23:09:31 +0100

    part18 key inputs

--- part_18 - Drum kit/index.js
+++ part_18 - Drum kit/index.js
@@ -2,6 +2,53 @@ var numberOfButtons = document.querySelectorAll(".drum").length
 
 for (var i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("button clicked")
+        // alert("button clicked")
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
     })
 }
+
+
+document.addEventListener("keydown", function(event) {
+    // alert("a key was pressed")
+    // console.log(event);
+    var keyPressed = event.key;
+    makeSound(keyPressed);
+});
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;  
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        default: console.log(buttonInnerHTML); 
+    }
+}
+
