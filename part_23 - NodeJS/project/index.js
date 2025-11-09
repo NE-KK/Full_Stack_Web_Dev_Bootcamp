8d34d07c6eaf76a9ef7aeae2b7f03c9b20f7a2a3
NE-KK <134157533+NE-KK@users.noreply.github.com>
2025-11-09 09:23:32 +0100

    part_23

--- part_23 - NodeJS/project/index.js
+++ part_23 - NodeJS/project/index.js
@@ -1,54 +1,27 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
-
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'url',
-      message: 'Enter a URL to generate a QR code:',
-    },
-    {
-      type: 'input',
-      name: 'filename',
-      message: 'Enter a filename to save the QR code image (without extension):',
+      message: "Type in your URL: ",
+      name: "URL",
     },
   ])
   .then((answers) => {
-    const qr = require('qr-image');
-    const fs = require('fs');
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
 
-    const url = answers.url;
-    const filename = answers.filename;
-
-    // Generate QR code image
-    const qrImage = qr.image(url, { type: 'png' });
-    const qrImagePath = `${filename}.png`;
-    const writeStream = fs.createWriteStream(qrImagePath);
-    qrImage.pipe(writeStream);
-
-    writeStream.on('finish', () => {
-      console.log(`QR code image saved as ${qrImagePath}`);
-    });
-
-    // Save URL to a text file
-    const urlFilePath = `${filename}.txt`;
-    fs.writeFile(urlFilePath, url, (err) => {
-      if (err) {
-        console.error('Error writing URL to file:', err);
-      } else {
-        console.log(`URL saved to ${urlFilePath}`);
-      }
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
     });
   })
   .catch((error) => {
     if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment  
+      // Prompt couldn't be rendered in the current environment
     } else {
       // Something else went wrong
     }
